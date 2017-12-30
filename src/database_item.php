15395e98904f2928433690682c62d422e32a2bbe
Unknown <bernat_galmes@hotmail.com>
2017-12-30 19:59:11 +0100

    abstract database_item class

--- src/database_item.php
+++ src/database_item.php
@@ -8,8 +8,15 @@ namespace PhpGene;
  *
  * @package PhpGene
  */
-class database_item
+abstract class database_item
 {
+    const ID_NAME = "id";
+
+    /**
+     * @var string Database item table name
+     */
+    protected $_db_table;
+
     /**
      * @var array item attributes
      * key: attribute name
@@ -18,12 +25,54 @@ class database_item
     protected $_data;
 
     /**
+     * database_item constructor.
+     * @param array|int|null $item data(associative array) or identifier(int) of the item to build.
+     *                              If null, data is not filled.
+     * @param string $db_table Database table of the item
+     * @throws \Exception item not found in database
+     */
+    function __construct($item, $db_table)
+    {
+        $this->_db_table = $db_table;
+        if (is_array($item)){ // item is data
+            $this->_data = $item;
+
+        }elseif (is_numeric($item)){ //item is identifier
+            if (!$this->__build_from_db($item)){
+                throw new \Exception("item not found in database");
+            }
+        }
+    }
+
+    /**
+     * Your own logic to fill the $_data attribute from database
+     * @param int|string $id identifier of item in database
+     * @return bool
+     */
+    protected abstract function __build_from_db($id);
+
+    /**
+     * Update the database data with the current object data
+     * @return true
+     * @throws \Exception item can't be updated
+     */
+    public abstract function update();
+
+    /**
+     * Remove the item in the database
+     * @return bool
+     * @throws \Exception item can't be deleted
+     */
+    public abstract function remove();
+
+
+    /**
      * Get the identifier of the item
      * @return int|string Id of the item
      */
     public function getID()
     {
-        return $this->getAttr('id');
+        return $this->getAttr(self::ID_NAME);
     }
 
     /**
